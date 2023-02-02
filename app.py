433453942c646fce5bd19206e6ec833015329de0
Alex <alecolivaw@gmail.com>
2023-02-02 20:01:43 +0400

    Commit

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def admin_only(f):
     return decorated_function
 
 
-class BlogPost(db.Model):
+class BlogPost(Base, db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(250), nullable=False)
@@ -47,14 +47,17 @@ class BlogPost(db.Model):
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
+    users = relationship("User", back_populates="blog_posts")
 
 
-class User(UserMixin, db.Model):
+class User(Base, UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(100), nullable=False)
     user_email = db.Column(db.String(100), unique=True, nullable=False)
     user_password = db.Column(db.String(100), nullable=False)
+    blog_post = relationship("BlogPost", back_populates="users")
+    blog_post_id = db.Column(db.Integer, db.ForeignKey(blog_post))
 
 
 with app.app_context():
